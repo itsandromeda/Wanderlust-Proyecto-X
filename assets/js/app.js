0334ba3f104da06912f1c4f5e9d35a63328273ba
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-25 14:36:20 -0500

    video/imagenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,15 +14,36 @@ dropZone.addEventListener('drop', function(e) {
     var files = e.dataTransfer.files; // Array of all files
 
     for (var i = 0, file; file = files[i]; i++) {
+        if (file.type.match(/video.*/)) {
+            var reader = new FileReader();
+
+            reader.onload = function(e2) {
+                var contenedor = document.createElement("video");
+                contenedor.setAttribute("controls","");
+                contenedor.setAttribute("class", "mi-contenedor-video");
+                var img = document.createElement('source');
+                img.setAttribute("class","imagen");
+                img.src = e2.target.result;
+                var myCaption = document.createElement("nav");
+                myCaption.setAttribute("class", "heart");
+                contenedor.appendChild(img);
+                contenedor.appendChild(myCaption);
+                dropZone.appendChild(contenedor);
+
+            }
+
+            reader.readAsDataURL(file); // start reading the file data.
+        }
+
         if (file.type.match(/image.*/)) {
             var reader = new FileReader();
 
             reader.onload = function(e2) {
                 // finished reading file data.
                 var contenedor = document.createElement("figure");
-                contenedor.setAttribute("class", "mi-contenedor");
+                contenedor.setAttribute("class", "mi-contenedor-imagen");
                 var img = document.createElement('img');
-                // img.setAttribute("class","imagen");
+                img.setAttribute("class","imagen");
                 img.src = e2.target.result;
                 var myCaption = document.createElement("figcaption");
                 myCaption.setAttribute("class", "heart");
