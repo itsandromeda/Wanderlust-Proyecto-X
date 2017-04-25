62fdf9991b1213b14d598dadff08c2badb3fbf4f
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-25 12:25:35 -0500

    drag/drop files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,38 @@
-function myFunction(e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    if(x>1021){
-      document.getElementsByClassName("menu")[0].style.width="19em";
-    }else if(x<1021){
-      document.getElementsByClassName("menu")[0].style.width="1.5em";
-    }
+var dropZone = document.getElementById('dropZone');
 
-}
+// Optional.   Show the copy icon when dragging over.  Seems to only work for chrome.
+dropZone.addEventListener('dragover', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+});
+
+// Get file data on drop
+dropZone.addEventListener('drop', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    var files = e.dataTransfer.files; // Array of all files
+
+    for (var i = 0, file; file = files[i]; i++) {
+        if (file.type.match(/image.*/)) {
+            var reader = new FileReader();
+
+            reader.onload = function(e2) {
+                // finished reading file data.
+                var contenedor = document.createElement("figure");
+                contenedor.setAttribute("class", "mi-contenedor");
+                var img = document.createElement('img');
+                // img.setAttribute("class","imagen");
+                img.src = e2.target.result;
+                var myCaption = document.createElement("figcaption");
+                myCaption.setAttribute("class", "heart");
+                contenedor.appendChild(img);
+                contenedor.appendChild(myCaption);
+                dropZone.appendChild(contenedor);
+
+            }
+
+            reader.readAsDataURL(file); // start reading the file data.
+        }
+    }
+});
