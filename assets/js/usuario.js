041d4e29bd189bb7d6549a30fb8c406dfb0a85c2
itsandromeda <itsandromeda@gmail.com>
2017-04-26 15:40:58 -0500

    Login site

--- assets/js/usuario.js
+++ assets/js/usuario.js
@@ -1,25 +1,54 @@
-var usuarios=[{nombre:"Leidy" , correo:"ross@lab.es" , contraseña:"123", source:"" },
-              {nombre:"Ruth" , correo:"ruth@lab.es" , contraseña:"456" },
-              {nombre: "Gladys" , correo:"gla@lab.es" , contraseña:"789" },
-              {nombre:"Vanny", correo: "va@lab.es" , contraseña:"1011" },
-              {nombre:"Flor" , correo:"flor@lab.es" , contraseña:"1012" }];
-var correo=document.getElementById("correo");
-var pass=document.getElementById("pass");
-localStorage.setItem("user",JSON.stringify(usuarios));
+var usuarios = [{
+        nombre: "Leidy",
+        correo: "ross@lab.es",
+        contraseña: "123",
+        source: "../assets/images/gabriella_ram.jpg"
+    },
+    {
+        nombre: "Annia",
+        correo: "itsandromeda@gmail.com",
+        contraseña: "321",
+        source: "assets/images/annia.jpg"
+    },
+    {
+        nombre: "Ruth",
+        correo: "ruth@lab.es",
+        contraseña: "456"
+    },
+    {
+        nombre: "Gladys",
+        correo: "gla@lab.es",
+        contraseña: "789"
+    },
+    {
+        nombre: "Vanny",
+        correo: "va@lab.es",
+        contraseña: "1011"
+    },
+    {
+        nombre: "Flor",
+        correo: "flor@lab.es",
+        contraseña: "1012"
+    }];
+var correo = document.getElementById("username");
+var pass = document.getElementById("pw");
 
-document.getElementById("loguear").addEventListener("click", function(){
-myUser=JSON.parse(localStorage.getItem("user"));
-var autentica=myUser.filter(function(elemento){
-  return elemento.correo==correo.value;
-})[0];
-  localStorage.setItem("autentica",autentica.nombre);
-if (autentica!=null) {
-  if(autentica.correo==correo.value&& autentica.contraseña==pass.value){
-  window.location="index.html";
-  }else {
-    alert("La contraseña es incorrecta");
-  }
-}else {
-  alert("El usuario no esta autorizado");
-}
+localStorage.setItem("user", JSON.stringify(usuarios));
+document.getElementById("sign-in").addEventListener("click", function () {
+    myUser = JSON.parse(localStorage.getItem("user"));
+    var autentica = myUser.filter(function (elemento) {
+        return elemento.correo == correo.value;
+    })[0];
+
+    localStorage.setItem("autentica", autentica.nombre);
+    var recursoFoto = autentica.source;
+    if (autentica != null) {
+        if (autentica.correo == correo.value && autentica.contraseña == pass.value) {
+            window.location = "index.html";
+        } else {
+            alert("La contraseña es incorrecta");
+        }
+    } else {
+        alert("El usuario no esta autorizado");
+    }
 });
